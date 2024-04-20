43c9d53afdeea127c3b5aedd7478bf29c0108415
Murilo <dixmurilox@gmail.com>
2024-04-20 16:00:36 -0300

    feat: calling the method to shuffle the answers in the selectedQuestion

--- pages/api/questions/[id].ts
+++ pages/api/questions/[id].ts
@@ -1,6 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import questions from "../questionsBank";
-
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (typeof req.query.id === "undefined") {
     return res.status(400).json({ error: "Missing ID" });
@@ -10,7 +9,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const questionOrNothing = questions.filter(question => question.id == selectedId);
 
   if (questionOrNothing.length === 1) {
-    const selectedQuestion = questionOrNothing[0];
+    const selectedQuestion = questionOrNothing[0].shuffleAnswers();
     res.status(200).json(selectedQuestion);
   } else {
     res.status(204).send({ msg: "No content" });
