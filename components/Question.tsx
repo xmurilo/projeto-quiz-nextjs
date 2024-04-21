8d9674d2e777c1f8d3f133c794df01979149b57b
Murilo <dixmurilox@gmail.com>
2024-04-21 13:49:39 -0300

    feat: function to render the Answers

--- components/Question.tsx
+++ components/Question.tsx
@@ -1,6 +1,7 @@
 import QuestionModel from "@/model/question";
 import styles from "../styles/Question.module.css";
 import Statement from "./Statement";
+import Answer from "./Answer";
 
 interface QuestionProps {
   value: QuestionModel;
@@ -8,9 +9,24 @@ interface QuestionProps {
 
 export default function Question(props: QuestionProps) {
   const question = props.value;
+
+  function renderAnswers() {
+    return question.answers.map((answer, i) => {
+      return (
+        <Answer
+          key={`${question.id}`}
+          value={answer}
+          index={i}
+          letter={"A"}
+          backgroundColorLetter={"#F2C866"}
+        />
+      )
+    })
+  }
   return (
     <div className={styles.question}>
       <Statement text={question.statement} />
+      {renderAnswers()}
     </div>
   );
 }
