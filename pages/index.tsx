c63e1fa2b9e205ca20bb0610094b0117095e271b
Murilo <dixmurilox@gmail.com>
2024-04-21 15:25:34 -0300

    feat: strutucre of page, with mock, and a function to get the response

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,33 @@
 import Question from "@/components/Question";
 import AnswerModel from "@/model/answer";
 import QuestionModel from "@/model/question";
+import { useState } from "react";
+
+const questionMock = new QuestionModel(1, "Melhor cor?", [
+  AnswerModel.isCorrect("Roxo"),
+  AnswerModel.isWrong("Vermelha"),
+  AnswerModel.isWrong("Azul"),
+  AnswerModel.isWrong("Amarelo"),
+]);
 
 export default function Home() {
-  const questionTest = new QuestionModel(1, "Melhor cor?", [
-    AnswerModel.isCorrect("Roxo"),
-    AnswerModel.isWrong("Vermelha"),
-    AnswerModel.isWrong("Azul"),
-    AnswerModel.isWrong("Amarelo"),
-  ]);
-  
-  return <Question value={questionTest} />;
+  const [question, setQuestion] = useState<QuestionModel>(questionMock);
+
+  function onResponse(index: number): void {
+    console.log(index);
+    setQuestion(question.answerWith(index));
+  }
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Question value={question} onResponse={onResponse} />
+    </div>
+  );
 }
