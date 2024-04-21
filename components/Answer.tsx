2dc2c5dec33c18a15d4d066d212f1a5cd6f14a11
Murilo <dixmurilox@gmail.com>
2024-04-21 13:59:38 -0300

    feat: backgroundcolor with props and renaming propertie of interface of props

--- components/Answer.tsx
+++ components/Answer.tsx
@@ -1,28 +1,28 @@
-import AnswerModel from "@/model/answer"
-import styles from "../styles/Answer.module.css"
+import AnswerModel from "@/model/answer";
+import styles from "../styles/Answer.module.css";
 interface AnswerProps {
-  value: AnswerModel
-  index: number
-  letter: string
-  backgroundColorLetter: string
+  value: AnswerModel;
+  index: number;
+  letter: string;
+  backgroundColorLetter: string;
 }
 
-export default function Answer(props: AnswerProps){
-  const answer = props.value
+export default function Answer(props: AnswerProps) {
+  const answer = props.value;
 
   return (
     <div className={styles.answer}>
       <div className={styles.contentAnswer}>
         <div className={styles.front}>
-          <div className={styles.letter}>
+          <div className={styles.letter} 
+          style={{ backgroundColor: props.backgroundColorLetter }}>
             {props.letter}
           </div>
-          <div className={styles.letter}>
-            {answer.value}
-          </div>
+          <div className={styles.value}>{answer.value}</div>
         </div>
+        
         <div className={styles.back}></div>
       </div>
     </div>
-  )
+  );
 }
