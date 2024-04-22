84059bbc3bdb945536928ba40efacf000f6c424e
Murilo <dixmurilox@gmail.com>
2024-04-22 00:01:51 -0300

    feat: Timer component

---
+++ components/Timer.tsx
@@ -0,0 +1,24 @@
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import styles from "../styles/Timer.module.css";
+
+interface TimerProps {
+  duration: number;
+  timeIsOver: () => void;
+}
+
+export default function Timer(props: TimerProps) {
+  return (
+    <div className={styles.timer}>
+      <CountdownCircleTimer
+        size={120}
+        isPlaying
+        duration={props.duration}
+        onComplete={props.timeIsOver}
+        colors={["#008000", "#FFFF00", "#FF0000", "#FF0000"]}
+        colorsTime={[10, 6, 4, 0]}
+      >
+        {({ remainingTime }) => remainingTime}
+      </CountdownCircleTimer>
+    </div>
+  );
+}
