c2ab03a5e560bf1e85c9be669dde8ea2ea54148f
Murilo <dixmurilox@gmail.com>
2024-04-30 17:08:01 -0300

    feat: adding class to show the effect of transition

--- components/Answer.tsx
+++ components/Answer.tsx
@@ -10,32 +10,29 @@ interface AnswerProps {
 
 export default function Answer(props: AnswerProps) {
   const answer = props.value;
-
+  const answerRevealed = answer.revealed ? styles.answerRevealed : "";
   return (
     <div className={styles.answer} onClick={() => props.onResponse(props.index)}>
-      <div className={styles.contentAnswer}>
-        {!answer.revealed ? (
-          <div className={styles.front}>
-            <div className={styles.letter} style={{ backgroundColor: props.backgroundColorLetter }}>
-              {props.letter}
+      <div className={`${answerRevealed} ${styles.contentAnswer} `}>
+        <div className={styles.front}>
+          <div className={styles.letter} style={{ backgroundColor: props.backgroundColorLetter }}>
+            {props.letter}
+          </div>
+          <div className={styles.value}>{answer.value}</div>
+        </div>
+        <div className={styles.back}>
+          {answer.isCorrect ? (
+            <div className={styles.isRight}>
+              <div>A resposta certa é...</div>
+              <div className={styles.value}>{answer.value}</div>
             </div>
-            <div className={styles.value}>{answer.value}</div>
-          </div>
-        ) : (
-          <div className={styles.back}>
-            {answer.isCorrect ? (
-              <div className={styles.isRight}>
-                <div>A resposta certa é...</div>
-                <div className={styles.value}>{answer.value}</div>
-              </div>
-            ) : (
-              <div className={styles.wrong}>
-                <div>A resposta informada está errada...</div>
-                <div className={styles.value}>{answer.value}</div>
-              </div>
-            )}
-          </div>
-        )}
+          ) : (
+            <div className={styles.wrong}>
+              <div>A resposta informada está errada...</div>
+              <div className={styles.value}>{answer.value}</div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
