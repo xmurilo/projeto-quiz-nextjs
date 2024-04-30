772c57585ff6a984bae7e7c3cf8c917e31da4480
Murilo <dixmurilox@gmail.com>
2024-04-30 16:16:36 -0300

    feat: adding total, rightAnswers percent in the screen

--- pages/result.tsx
+++ pages/result.tsx
@@ -1,7 +1,22 @@
-export default function result() {
+import styles from "../styles/Result.module.css";
+import { useRouter } from "next/router";
+
+export default function Result() {
+  const router = useRouter();
+
+  if(typeof router.query.total === "undefined" || typeof router.query.rightAnswers === "undefined") {
+    return <div>Missing query parameters</div>;
+  }
+
+  const total = +router.query.total
+  const rightAnswers = +router.query.rightAnswers
+  const percent = Math.round((rightAnswers / total) * 100)
   return (
-    <div>
+    <div className={styles.result}>
       <h1>Resultado</h1>
+      <span>{total}</span>
+      <span>{rightAnswers}</span>
+      <span>{`${percent}%`}</span>
     </div>
   );
 }
